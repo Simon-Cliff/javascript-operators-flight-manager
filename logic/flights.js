2a76b760f937decf550e19a88c20b30a7c8668e1
Simon Clifford <simon.clifford85@gmail.com>
2021-11-04 14:02:10 +1100

    completed question 1

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,39 @@
 function Flights() {
 
-}
+    function calculateNumberOfFlights(passengers, capacity) {
+        let passengers = document.getElementById('passengers').value;
+        let capacity = document.getElementById('capacity').value;
+        let negPassengerMsg = "The number of passengers must be a positive integer value";
+        let negCapacityMsg = "The capacity of the flight must be a positive integer value";
+        try {
+            if (passengers > 0) {
+                console.log("Positive Passengers");
+            }
+            else {throw Error (negPassengerMsg);
+            }                    
+            
+        } catch (error) {
+            console.error("Error",negPassengerMsg); 
+        }
+        try {
+            if (capacity > 0) {
+                console.log("Positive Capacity");
+            }
+            else {throw Error (negCapacityMsg);
+            }                    
+            
+        } catch (error) {
+            console.error("Error",negCapacityMsg); 
+        } 
+    }
+    if (passengers % capacity == 0) {
+        return passengers / capacity;
+    }
 
+    if (passengers % capacity > 0) {
+    return Math.ceil(passengers / capacity);
+    }
+  
+    return {calculateNumberOfFlights};
+}
+module.exports = Flights();
--- logic/frontend.js
+++ logic/frontend.js
@@ -13,6 +13,9 @@ function onCalculateNumberOfFlights() {
     } catch (error) {
         document.getElementById('flights').innerHTML = error; 
     } 
+    finally {
+        console.log("Number of passengers: " + passengers, ", " + "Plane capacity: " + capacity);
+    }
 }
 
 function onCalculateTotalFinalPrice(object) {
