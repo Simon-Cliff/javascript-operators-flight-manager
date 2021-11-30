dc5571fe6f611584965ee4ee2f46e2a0f288c42d
Simon Clifford <simon.clifford85@gmail.com>
2021-12-01 09:27:40 +1100

    Complete module 3

--- logic/passengers.js
+++ logic/passengers.js
@@ -11,7 +11,6 @@ function Passengers() {
         }
         return passengersNumber;
      }
-
      function distributeAllSeatsToAllPassengers(vipPassengers, regularPassengers, nrOfFlights, 
         businessSeatsPerFlight, economySeatsPerFlight) {
         
@@ -19,30 +18,23 @@ function Passengers() {
         regularPassengersWithBusinessSeats=0, regularPassengersWithEconomySeats=0;
         let availableBusinessSeats = nrOfFlights * businessSeatsPerFlight;
         let availableEconomySeats = nrOfFlights * economySeatsPerFlight;
-
         var vipBusinessConfiguration = {passengers:vipPassengers, seats:availableBusinessSeats};
         vipPassengersWithBusinessSeats = updateConfiguration(vipBusinessConfiguration, businessSeatsPerFlight);
-
         var vipEconomyConfiguration = {passengers:vipBusinessConfiguration.passengers, seats:availableEconomySeats};
         vipPassengersWithEconomySeats = updateConfiguration(vipEconomyConfiguration, economySeatsPerFlight);
-
         var regularBusinessConfiguration = {passengers:regularPassengers, seats:vipBusinessConfiguration.seats};
         regularPassengersWithBusinessSeats = updateConfiguration(regularBusinessConfiguration, businessSeatsPerFlight);
-
         var regularEconomyConfiguration = {passengers:regularBusinessConfiguration.passengers, seats:vipEconomyConfiguration.seats};
         regularPassengersWithEconomySeats = updateConfiguration(regularEconomyConfiguration, economySeatsPerFlight);
-
         return {vipPassengersWithBusinessSeats:vipPassengersWithBusinessSeats,
                 vipPassengersWithEconomySeats:vipPassengersWithEconomySeats, regularPassengersWithBusinessSeats:regularPassengersWithBusinessSeats,
                 regularPassengersWithEconomySeats:regularPassengersWithEconomySeats};
     }
-
     function updateConfiguration(configuration, seatsPerFlight) {
         let passengersWithSeats = 0;
         while (configuration.passengers > 0) {
             if (configuration.seats > 0) {
                if (configuration.passengers >= configuration.seats) {
-
                     if (configuration.seats > configuration.seatsPerFlight) {
                         configuration.passengers -= seatsPerFlight;
                         configuration.seats -= seatsPerFlight;
@@ -63,9 +55,6 @@ function Passengers() {
         }
         return passengersWithSeats;
     }
-
      return {checkFlightCapacity, distributeAllSeatsToAllPassengers};
-
 }
-
 module.exports = Passengers();
